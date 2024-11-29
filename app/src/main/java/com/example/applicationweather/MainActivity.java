29c044a52611ca68e3ee86f486d0b26473d1b5d1
shelleyng <shelleyn208@gmail.com>
2024-11-29 09:10:42 -0500

    third

--- app/src/main/java/com/example/applicationweather/MainActivity.java
+++ app/src/main/java/com/example/applicationweather/MainActivity.java
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -60,9 +63,33 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.recherche,menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)menuItem.getActionView();
+        searchView.setQueryHint("Write the name of the city");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                maVille = query;
+                afficher();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
     public void afficher()
     {
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=f214627d726036042b132bf68b40acf9";
+        String url = "https://api.openweathermap.org/data/2.5/weather?q="+ maVille + "&appid=f214627d726036042b132bf68b40acf9";
         JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response)
@@ -73,13 +100,10 @@ public class MainActivity extends AppCompatActivity {
                     Object name = response.get("name");
                     JSONObject object = array.getJSONObject(0);
                     Log.d("Tag","resultat = " +array.toString()); // log resultat du logcat
-
                     mcity.setText(name.toString());
                     mtemp.setText(String.valueOf(Math.ceil(Double.parseDouble(main_object.get("temp").toString()) - 273.15))); // pour mettre en degreés et arrondir
                     mdescription.setText(object.get("main").toString()); // mettre le text du logcat "description"
                     String icon = object.getString("icon");
-                    String city = object.getString("name");
-                    String description = object.getString("description");
 
                     //gestion de l'image
                     String imageUri="http://openweathermap.org/img/w/" + icon + ".png";
